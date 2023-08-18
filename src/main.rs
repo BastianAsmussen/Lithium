f9681a3cc508cb5140853577bc3fba9787109028
Bastian Asmussen <bastianasmussen@gmail.com>
2023-08-18 02:33:04 +0200

    Update main.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod lang;
+mod util;
+
 fn main() {
-    println!("Hello, world!");
+    // Read the file parsed by the user.
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <file>", args[0]);
+        return;
+    }
+
+    let mut lithium = lang::Lithium::new();
+
+    lithium.run_file(&args[1]);
 }
