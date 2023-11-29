19d4735ae425e2e002b19f02b1ec1535c350b9f7
Bastian Asmussen <bastianasmussen@tutanota.com>
2023-11-29 18:57:33 +0100

    Updated examples and benchmarks.

--- lexer/benches/lex_large_file.rs
+++ lexer/benches/lex_large_file.rs
@@ -2,7 +2,11 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 #[allow(clippy::unwrap_used)]
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("lex large file", |b| {
+    let mut group = c.benchmark_group("lex large file");
+
+    group.sample_size(10);
+    group.measurement_time(std::time::Duration::from_secs(60));
+    group.bench_function("lex large file", |b| {
         b.iter(|| {
             let contents = std::fs::read_to_string("../examples/large_file.lt").unwrap();
 
