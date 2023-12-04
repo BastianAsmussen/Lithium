8c1c1c9bada29c7200d015022b5e090b100677de
BastianAsmussen <bastian@asmsusen.tech>
2023-12-04 13:49:37 +0100

    Updated `measurement_time` for `lex_large_file`.

--- lexer/benches/lex_large_file.rs
+++ lexer/benches/lex_large_file.rs
@@ -5,7 +5,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("lex large file");
 
     group.sample_size(10);
-    group.measurement_time(std::time::Duration::from_secs(60));
+    group.measurement_time(std::time::Duration::from_secs(75));
     group.bench_function("lex large file", |b| {
         b.iter(|| {
             let contents = std::fs::read_to_string("../examples/large_file.lt").unwrap();
