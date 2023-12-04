b3d68b20f2f82560925f76aeef5c1ad6fc041eca
BastianAsmussen <bastian@asmsusen.tech>
2023-12-04 14:07:03 +0100

    Added argument handling.
    - Now a user can give an input file to compile (for now this is just lexing).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::Parser;
+use std::fs::{read_to_string};
+
+/// The Lithium compiler CLI.
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The input file.
+    #[arg(short, long)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let contents = match read_to_string(args.file) {
+        Ok(contents) => contents,
+        Err(why) => {
+            eprintln!("Failed to read file: {why}");
+
+            std::process::exit(1);
+        }
+    };
+
+    let mut lexer = lexer::Lexer::new(&contents);
+    let tokens = match lexer.tokenize() {
+        Ok(tokens) => tokens,
+        Err(why) => {
+            eprintln!("Failed to tokenize input: {why}");
+
+            std::process::exit(1);
+        }
+    };
+
+    println!("{tokens:#?}");
 }
