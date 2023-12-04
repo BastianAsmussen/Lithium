a7f2d37e8a5f6bcf545c7572867d5be54fd0f0af
BastianAsmussen <bastian@asmsusen.tech>
2023-12-04 13:51:32 +0100

    Prepare for parser!

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,1 +1,29 @@
+use lexer::token::Token;
 
+/// A parser for generating an AST from a stream of tokens.
+///
+/// # Fields
+///
+/// * `tokens` - The stream of tokens to parse.
+/// * `current` - The current index of the token stream.
+#[derive(Debug)]
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+}
+
+impl Parser {
+    /// Creates a new parser from a stream of tokens.
+    ///
+    /// # Arguments
+    ///
+    /// * `tokens` - The stream of tokens to parse.
+    ///
+    /// # Returns
+    ///
+    /// * `Self` - The new parser.
+    #[must_use]
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self { tokens, current: 0 }
+    }
+}
