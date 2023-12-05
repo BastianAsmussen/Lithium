86fd1ce1da6da024ea5e117474e59fc563b28e6f
BastianAsmussen <bastian@asmsusen.tech>
2023-12-05 13:38:34 +0100

    Implement parser errors.

--- parser/src/errors.rs
+++ parser/src/errors.rs
@@ -1,12 +1,29 @@
+use std::num::ParseFloatError;
 use thiserror::Error;
 
-/// An parsing error.
+/// A parsing error.
+///
+/// # Variants
+///
+/// * `UnexpectedToken` - An unexpected token was encountered.
+/// * `FunctionParameterLength` - A function has too many parameters.
+/// * `InvalidAssignmentTarget` - An invalid assignment target was encountered.
+/// * `ParseFloat` - Failed to parse a float.
+/// * `UnexpectedEndOfFile` - Unexpected end of file.
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("Invalid token at line {line}, column {column}! ({message})")]
-    InvalidToken {
-        message: String,
+    #[error("Unexpected token at line {line} and column {column}: {message}")]
+    UnexpectedToken {
         line: usize,
         column: usize,
+        message: String,
     },
+    #[error("Invalid function parameter length at line {line} and column {column}")]
+    FunctionParameterLength { line: usize, column: usize },
+    #[error("Invalid assignment target at line {line} and column {column}!")]
+    InvalidAssignmentTarget { line: usize, column: usize },
+    #[error("Failed to parse float!")]
+    ParseFloat(#[from] ParseFloatError),
+    #[error("Unexpected end of file!")]
+    UnexpectedEndOfFile,
 }
